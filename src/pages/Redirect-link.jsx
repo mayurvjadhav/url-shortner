2f86a6908b52b3af78d342d06be7705dc6001bbd
mayurvjadhav <133495039+mayurvjadhav@users.noreply.github.com>
2025-09-29 14:48:47 +0530

    Update Redirect-link.jsx

--- src/pages/Redirect-link.jsx
+++ src/pages/Redirect-link.jsx
@@ -1,6 +1,6 @@
-import { storeClicks } from "@/db/apiClicks";
-import { getLongUrl } from "@/db/apiUrls";
-import useFetch from "@/hooks/use-fetch";
+import { storeClicks } from "../db/apiClicks";
+import { getLongUrl } from "../db/apiUrls";
+import UseFetch from "../hooks/Use-fetch";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { BarLoader } from "react-spinners";
@@ -8,22 +8,22 @@ import { BarLoader } from "react-spinners";
 export const RedirectLink = () => {
   const { id } = useParams();
 
-  const { loading, data, fn } = useFetch(getLongUrl, id);
+  const { loading, data, fn } = UseFetch(getLongUrl, id);
 
-  const { loading: loadingStats, fn: fnStats } = useFetch(storeClicks, {
+  const { loading: loadingStats, fn: fnStats } = UseFetch(storeClicks, {
     id: data?.id,
     originalUrl: data?.original_url,
   });
 
   useEffect(() => {
     fn();
-  }, []);
+  }, [fn]);
 
   useEffect(() => {
     if (!loading && data) {
       fnStats();
     }
-  }, [loading]);
+  }, [data, fnStats, loading]);
 
   if (loading || loadingStats) {
     return (
