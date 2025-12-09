7b68c607e26ad996ad4bc1fa149a7da05091230a
Mayur <jadhavmayur26062001@gmail.com>
2025-12-09 12:00:30 +0530

    code updated

--- src/components/ui/require-auth.jsx
+++ src/components/ui/require-auth.jsx
@@ -2,7 +2,7 @@
 
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { UrlState } from "@/context";
+import { UrlState } from "../../Context";
 import { BarLoader } from "react-spinners";
 
 function RequireAuth({ children }) {
