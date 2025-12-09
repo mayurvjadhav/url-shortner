71f3cb4916654f04a7423db09424bf7b441d1391
Mayur <jadhavmayur26062001@gmail.com>
2025-12-09 12:14:54 +0530

    code updated

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  //for Render
+  preview: {
+    host: true,
+    port: 8080,
+    allowedHosts: ["urlclipr.onrender.com"],
   },
 });
